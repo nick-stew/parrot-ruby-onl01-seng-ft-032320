495ccc50472552a9dbcec72a549f5d709ee04952
John Stewart <jnicklausstewart@yahoo.com>
2020-03-24 21:17:39 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def #parrot
+def parrot(string="Squawk!")
 
-puts "The Parrot's Name is?"
-
-return "Squawk!"
+puts "#{string}"
 
 end
